165b03154dedff27d803f58117408224c0f1809d
robiulalam76 <robiulalam762400@gmail.com>
2023-09-01 11:35:16 +0600

    project ready

---
+++ src/app/routes/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const router = express.Router();
+
+// const moduleRoutes = [
+//   {
+//     path: '/academic-semester',
+//     route: AcademicSemesterRoutes,
+//   }
+// ];
+
+// moduleRoutes.forEach(route => router.use(route.path, route.route));
+
+export default router;
