d951e655403475cab2a29cc05cc7d7aad88c5df5
robiulalam76 <robiulalam762400@gmail.com>
2023-09-01 12:19:52 +0600

    order model add

---
+++ prisma/migrations/20230901061935_order/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "order" (
+    "id" TEXT NOT NULL,
+    "orderedBooks" JSONB NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "order_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "order" ADD CONSTRAINT "order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
