289f5b7d333e8bb90cfd198cd694e2bc06573a55
robiulalam76 <robiulalam762400@gmail.com>
2023-09-01 11:53:09 +0600

    review and rating model add

---
+++ prisma/migrations/20230901055245_review_and_rating/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "reviewAndRating" (
+    "id" TEXT NOT NULL,
+    "review" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+    "bookId" TEXT NOT NULL,
+
+    CONSTRAINT "reviewAndRating_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "reviewAndRating" ADD CONSTRAINT "reviewAndRating_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviewAndRating" ADD CONSTRAINT "reviewAndRating_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
