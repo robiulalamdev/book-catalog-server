8ed389ba3b5f19faff3de4dc62e36528aa32ff61
robiulalam76 <robiulalam762400@gmail.com>
2023-09-02 11:13:35 +0600

    book controller added

---
+++ src/app/modules/book/book.controller.ts
@@ -0,0 +1,64 @@
+import { Book } from '@prisma/client';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { BookService } from './book.service';
+
+const create = catchAsync(async (req: Request, res: Response) => {
+  const result = await BookService.create(req.body);
+  sendResponse<Book>(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Books Create successfully',
+    data: result,
+  });
+});
+
+const getAll = catchAsync(async (req: Request, res: Response) => {
+  const result = await BookService.getAll();
+  sendResponse<Book[]>(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Books fetched successfully',
+    data: result,
+  });
+});
+
+const getSingle = catchAsync(async (req: Request, res: Response) => {
+  const result = await BookService.getSingle(req.params.id);
+  sendResponse<Book>(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Book retrieved successfully',
+    data: result,
+  });
+});
+
+const update = catchAsync(async (req: Request, res: Response) => {
+  const result = await BookService.update(req.params.id, req.body);
+  sendResponse<Book>(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Book updated successfully',
+    data: result,
+  });
+});
+
+const deleteSingle = catchAsync(async (req: Request, res: Response) => {
+  const result = await BookService.deleteSingle(req.params.id);
+  sendResponse<Book>(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Book deleted successfully',
+    data: result,
+  });
+});
+
+export const BookController = {
+  create,
+  getAll,
+  getSingle,
+  update,
+  deleteSingle,
+};
