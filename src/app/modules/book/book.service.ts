4a0a31de4913deac4f99689c753366f197153f3e
robiulalam76 <robiulalam762400@gmail.com>
2023-09-02 11:08:29 +0600

    book service crud service done

---
+++ src/app/modules/book/book.service.ts
@@ -0,0 +1,58 @@
+import { Book, PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const create = async (data: Book): Promise<Book> => {
+  const result = await prisma.book.create({
+    data,
+  });
+  return result;
+};
+
+const getSingle = async (id: string): Promise<Book | null> => {
+  const result = await prisma.book.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      category: true,
+    },
+  });
+  return result;
+};
+
+const getAll = async (): Promise<Book[]> => {
+  const result = await prisma.book.findMany({
+    include: {
+      category: true,
+    },
+  });
+  return result;
+};
+
+const update = async (id: string, Payload: Partial<Book>): Promise<Book> => {
+  const result = await prisma.book.update({
+    where: {
+      id: id,
+    },
+    data: Payload,
+  });
+
+  return result;
+};
+
+const deleteSingle = async (id: string): Promise<Book> => {
+  const result = await prisma.book.delete({
+    where: {
+      id: id,
+    },
+  });
+  return result;
+};
+
+export const BookService = {
+  create,
+  getSingle,
+  getAll,
+  update,
+  deleteSingle,
+};
